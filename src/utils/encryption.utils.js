5904efdc37575df70eda6ddd3980d561b62f043d
hebaa-elsayed <hebaa.elsayeddd@gmail.com>
2025-09-22 22:08:21 +0300

    Asymmetric Encryption Function

--- src/utils/encryption.utils.js
+++ src/utils/encryption.utils.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import crypto from "crypto";
+import fs from 'node:fs'
+import { buffer } from 'node:stream/consumers';
 
 
 const ENCRYPTION_KEY= Buffer.from(process.env.ENCRYPTION_KEY, 'utf-8' );
@@ -26,3 +28,52 @@ export function decrypt(text) {
 
 
 export default encrypt
+
+//Asymmetric encryption
+
+
+if (fs.existsSync('publicKey.pem') && fs.existsSync('privateKey.pem')){
+    console.log('Key already generated');
+    
+}else {
+    const {publicKey , privateKey} = crypto.generateKeyPairSync('rsa',{
+        modulusLength:2048,
+        publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        }
+    })
+
+    fs.writeFileSync('publicKey.pem', publicKey)
+    fs.writeFileSync('privateKey.pem', privateKey)
+}
+
+export const asymmetricEncryption = (text)=>{
+    const publicKey = fs.readFileSync('publicKey.pem', 'utf-8')
+    const bufferedText = buffer.from(text)
+    const encryptedData = crypto.publicEncrypt(
+        {
+            key:publicKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
+        },
+        bufferedText
+    )
+    return encryptedData.toString('hex')
+}
+
+
+export const asymmetricDecryption = (text)=>{
+    const privateKey = fs.readFileSync('privateKey.pem', 'utf-8')
+    const decryptedData = crypto.privateDecrypt(
+        {
+            key:privateKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
+        },
+        text
+    )
+    return decryptedData.toString('utf-8')
+}
